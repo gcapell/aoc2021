217744253c38a0801714501300b611bc382d18d8
Gary Capell <gary.capell@gmail.com>
2021-12-14 11:56:21 +1100

    refactor day13: discovered split_once and take_while

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,69 +1,64 @@
+use std::cmp::max;
 use std::collections::HashSet;
 
 type Point = (u32, u32);
 
 fn main() {
-    let mut lines = include_str!("input.txt").split_terminator('\n');
-    let mut points = HashSet::new();
-    loop {
-        let line = lines.next().unwrap();
-        if line.is_empty() {
-            break;
-        }
-        let coords = line
-            .split(',')
-            .map(str::parse)
-            .map(Result::unwrap)
-            .collect::<Vec<u32>>();
-        points.insert((coords[0], coords[1]));
-    }
+    let mut lines = include_str!("input.txt").lines();
+    let mut points: HashSet<Point> = (&mut lines)
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            let (a, b) = line.split_once(',').unwrap();
+            (a.parse().unwrap(), b.parse().unwrap())
+        })
+        .collect();
+
     for line in lines {
-        let mut line = line.strip_prefix("fold along ").unwrap().split('=');
-        let fold_fn = if line.next() == Some("x") {
-            foldx
+        let (axis, amount) = line
+            .strip_prefix("fold along ")
+            .unwrap()
+            .split_once('=')
+            .unwrap();
+        let amount = amount.parse().unwrap();
+        let deltas: Vec<(Point,Point)> = points
+            .iter()
+            .filter_map(|&p| foldp(p, axis, amount))
+            .collect();
+        for (insert, remove) in deltas.iter() {
+            points.insert(*insert);
+            points.remove(remove);
+        }
+    }
+    display(points);
+}
+
+fn display(points: HashSet<Point>) {
+    let (maxx, maxy) = points
+        .iter()
+        .fold((0, 0), |acc, p| (max(acc.0, p.0), max(acc.1, p.1)));
+
+    for y in 0..=maxy {
+        println!(
+            "{}",
+            (0..=maxx)
+                .map(|x| if points.contains(&(x, y)) { '#' } else { ' ' })
+                .collect::<String>()
+        );
+    }
+}
+
+fn foldp(c: Point, axis: &str, mid: u32) -> Option<(Point, Point)> {
+    if axis == "x" {
+        if c.0 < mid {
+            None
         } else {
-            foldy
-        };
-        let mid = line.next().unwrap().parse::<u32>().unwrap();
-
-        points = fold(&points, fold_fn, mid);
-    }
-		display(points);
-}
-
-fn display(points:HashSet<Point>){
-	let maxx = points.iter().map(|p|p.0).max().unwrap();
-	let maxy = points.iter().map(|p|p.1).max().unwrap();
-
-	for y in 0..=maxy {
-		let mut chars = Vec::new();
-		for x in 0..=maxx {
-			chars.push(if points.contains( &(x,y)) {'#'} else {' '});
-		}
-		println!("{}",chars.iter().collect::<String>());
-	}
-}
-
-fn fold(ps: &HashSet<Point>, f: fn(Point, u32) -> Point, mid: u32) -> HashSet<Point> {
-    let mut points = HashSet::new();
-    for p in ps {
-        points.insert(f(*p, mid));
-    }
-    points
-}
-
-fn foldy(c: Point, mid: u32) -> Point {
-    if c.1 < mid {
-        c
+            Some(((2 * mid - c.0, c.1), c))
+        }
     } else {
-        (c.0, 2 * mid - c.1)
-    }
-}
-
-fn foldx(c: Point, mid: u32) -> Point {
-    if c.0 < mid {
-        c
-    } else {
-        (2 * mid - c.0, c.1)
+        if c.1 < mid {
+            None
+        } else {
+            Some(((c.0, 2 * mid - c.1), c))
+        }
     }
 }
